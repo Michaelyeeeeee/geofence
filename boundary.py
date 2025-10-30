049c16f9f0a46d999b2b46ce15c58975bc24257e
Michael Wu <michaeldmwu@gmail.com>
2025-10-30 10:51:43 -0400

    new branch

--- boundary.py
+++ boundary.py
@@ -6,87 +6,87 @@ import math
 import machine
 import select
 
-
 # To determine if the coordinate/function q lies on the segment pr
-def onSegment(p: tuple, q: tuple, r: tuple) -> bool:
-    if ((q[0] <= max(p[0], r[0])) &
-            (q[0] >= min(p[0], r[0])) &
-            (q[1] <= max(p[1], r[1])) &
-            (q[1] >= min(p[1], r[1]))):
+def onSegment(p:tuple, q:tuple, r:tuple) -> bool:
+    
+    if((q[0] <= max(p[0], r[0])) &
+       (q[0] >= min(p[0], r[0])) &
+       (q[1] <= max(p[1], r[1])) &
+       (q[1] >= min(p[1], r[1]))):
         return True
     return False
 
-
-# Finding orientation
-def orientation(p: tuple, q: tuple, r: tuple) -> int:
-    val = (((q[1] - p[1]) * (r[0] - q[0])) - ((q[0] - p[0]) * (r[1] - q[1])))  # calculating slope
-
+#Finding orientation
+def orientation(p:tuple, q:tuple, r:tuple) -> int:
+    
+    val = (((q[1] - p[1]) * (r[0] - q[0])) - ((q[0] - p[0]) * (r[1] - q[1]))) #calculating slope
+    
     if (val > 0):
-        return 1  # positive slope is clockwise orientation
+        return 1 #positive slope is clockwise orientation
     elif (val < 0):
-        return 2  # negative slope is counterclockwise orientation
+        return 2 #negative slope is counterclockwise orientation
     else:
-        return 0  # collinear orientation
-
-
-# Determine if line segment p1q1 and p2q2 intersects
+        return 0 #collinear orientation
+    
+#Determine if line segment p1q1 and p2q2 intersects
 def doIntersect(p1, q1, p2, q2):
-    # looking for orientation
+    
+    #looking for orientation
     o1 = orientation(p1, q1, p2)
     o2 = orientation(p1, q1, q2)
     o3 = orientation(p2, q2, p1)
     o4 = orientation(p2, q2, q1)
-
-    # General Case: If the orientations are different, they intersect
+    
+    #General Case: If the orientations are different, they intersect
     if (o1 != o2) and (o3 != o4):
         return True
-
-    # Special Case (collinear): If x and y projection intersects, they intersect
-    if (o1 == 0) and (onSegment(p1, p2, q1)):  # p1, q1 and p2 are collinear and p2 lies on segment p1q1
+    
+    #Special Case (collinear): If x and y projection intersects, they intersect
+    if (o1 == 0) and (onSegment(p1, p2, q1)): #p1, q1 and p2 are collinear and p2 lies on segment p1q1
         return True
-
-    if (o2 == 0) and (onSegment(p1, q2, q1)):  # p1, q1 and q2 are collinear and q2 lies on segment p1q1
+    
+    if (o2 == 0) and (onSegment(p1, q2, q1)): #p1, q1 and q2 are collinear and q2 lies on segment p1q1
         return True
-
-    if (o3 == 0) and (onSegment(p2, p1, q2)):  # p2, q2 and p1 are collinear and p1 lies on segment p2q2
+    
+    if (o3 == 0) and (onSegment(p2, p1, q2)): #p2, q2 and p1 are collinear and p1 lies on segment p2q2
         return True
-
-    if (o4 == 0) and (onSegment(p2, q1, q2)):  # p2, q2 and q1 are collinear and q1 lies on segment p2q2
+    
+    if (o4 == 0) and (onSegment(p2, q1, q2)): #p2, q2 and q1 are collinear and q1 lies on segment p2q2
         return True
-
+    
     return False
 
-
-# Determine if the point p lies within the polygon
-def is_within_polygon(points: list, p: list) -> bool:
+#Determine if the point p lies within the polygon
+def is_within_polygon(points:list, p:list) -> bool:
+    
     n = len(points)
-    if n < 3:  # there must be at least 3 points/vertices in a polygon
+    if n < 3: #there must be at least 3 points/vertices in a polygon
         return False
-
-    extreme = (float('inf'), p[1])  # casts a ray from point to +inf in the x direction
-
-    decrease = 0  # To calculate number of points where y-coordinate of the polygon is equal to y-coordinate of the point
-    count = i = 0  # number of borders the horizontal ray crosses
-
+    
+    extreme = (float('inf'), p[1]) # casts a ray from point to +inf in the x direction
+    
+    decrease = 0 #To calculate number of points where y-coordinate of the polygon is equal to y-coordinate of the point
+    count = i = 0 # number of borders the horizontal ray crosses
+    
     while True:
-        next = (i + 1) % n  # next point
-
+        next = (i + 1) % n # next point
+        
         # increments number of double counted points
-        if (points[i][1] == p[1]):
+        if(points[i][1] == p[1]):
             decrease += 1
-
+        
         # checks if point is on border
         if (doIntersect(points[i], points[next], p, extreme)):
             if orientation(points[i], p, points[next]) == 0:
                 return onSegment(points[i], p, points[next])
-
+                           
             count += 1
-
+                           
         i = next
-
+        
         if (i == 0):
             break
         # subtract double counted case
         count -= decrease
-
+        
     return (count % 2 == 1)
--- main.py
+++ main.py
@@ -13,13 +13,10 @@ from boundary import onSegment, orientation, doIntersect, is_within_polygon
 '''
 Set of (x,y) coordinates
 '''
-
-
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-
 
 def dataReceive():
     print("listening...")
@@ -30,7 +27,7 @@ def dataReceive():
         print(value)
 
     # Temporary Geofence For testing purposes - Use computer-end-code.py for real Geofence Values
-    # value = "OUTER, 40.39123253146508, -86.82833099365233, 40.365601883498044, -86.97458648681639, 40.42417189314546, -87.00067901611327, 40.468065962237894, -86.85236358642577, 40.428353515662465, -86.79743194580077, INNER"
+    #value = "OUTER, 40.39123253146508, -86.82833099365233, 40.365601883498044, -86.97458648681639, 40.42417189314546, -87.00067901611327, 40.468065962237894, -86.85236358642577, 40.428353515662465, -86.79743194580077, INNER"
 
     coordinateList = value.split(",")
     innerBegin = coordinateList.index("INNER")
@@ -38,34 +35,33 @@ def dataReceive():
     outerList = [float(coordinateList[idx]) for idx in range(1, innerBegin)]
     innerList = [float(coordinateList[idx]) for idx in range(innerBegin + 1, len(coordinateList))]
 
-    outerList = [(outerList[i], outerList[i + 1]) for i in
-                 range(0, len(outerList) - 1, 2)]  # Groups the latitudes and longitudes together
-    innerList = [(innerList[i], innerList[i + 1]) for i in
-                 range(0, len(innerList) - 1, 2)]  # Groups the latitudes and longitudes together
+    outerList = [(outerList[i], outerList[i+1]) for i in range(0, len(outerList)-1,2)] # Groups the latitudes and longitudes together
+    innerList = [(innerList[i], innerList[i+1]) for i in range(0, len(innerList)-1,2)] # Groups the latitudes and longitudes together
 
     return outerList, innerList
 
-
 if __name__ == '__main__':
+    
+    
 
     loggingFileName = "test.txt"
 
     outerPolygon, innerPolygon = dataReceive()
 
-    i2c = busio.I2C(board.GP15, board.GP14, frequency=1000)  # Initializes I2C for the IMU
-    sensor = adafruit_bno055.BNO055_I2C(i2c)  # Initializes IMU
+    i2c = busio.I2C(board.GP15, board.GP14, frequency=1000)       # Initializes I2C for the IMU
+    sensor = adafruit_bno055.BNO055_I2C(i2c)                        # Initializes IMU
 
-    gps_uart = initialize_gps()  # Initializes GPS
+    gps_uart = initialize_gps()                                   # Initializes GPS
     lcd_uart = initialize_lcd(backlight_red=255, backlight_green=1, backlight_blue=255)
-
+    
     lcd_uart.write(b"Connecting to GPS...            ")  # For 16x2 LCD
-    # time.sleep(1.5) - Can add back in to display message for readability on LCD screen. The GPS sensor needs a few seconds to connect usually anyways.
-
+    #time.sleep(1.5) - Can add back in to display message for readability on LCD screen. The GPS sensor needs a few seconds to connect usually anyways. 
+    
     relay_on = machine.Pin(11, mode=machine.Pin.OUT)
     relay_on.value(1)
-
+    
     # Example polygon for testing
-
+    
     '''
     outerPolygon = [
     (40.430484, 86.915721),
@@ -73,7 +69,7 @@ if __name__ == '__main__':
     (40.430806, 86.916144),
     (40.430835, 86.916097)
     ]
-
+    
     innerPolygon = [
     (40.430484, 86.915721),
     (40.430454, 86.915769),
@@ -81,80 +77,85 @@ if __name__ == '__main__':
     (40.430835, 86.916097)
     ]
     '''
-
+    
     # CHANGE IMU SETTINGS HERE
-    imu_update_points = 7  # This value can be further optimized. If set to zero, there will be no IMU points (only GPS points).
-    imu_time_interval = 0.14  # This value can be further optimized. See IMU BNO055 documentation for minimum refresh rate.
-
-    # Initalize the GPS position and time trackers
+    imu_update_points = 7 # This value can be further optimized. If set to zero, there will be no IMU points (only GPS points).
+    imu_time_interval = 0.14 # This value can be further optimized. See IMU BNO055 documentation for minimum refresh rate.
+    
+    #Initalize the GPS position and time trackers
     velocity_x = 0
     velocity_y = 0
-
-    latitude_avg, longitude_avg = 0, 0
-    latitude_avg, longitude_avg = get_gps_location(gps_uart, lcd_uart)
+    
     gps_start_time, imu_start_time = time.ticks_ms(), time.ticks_ms()
+    
+    latitude_avg,longitude_avg = 0,0
+    latitude_avg,longitude_avg = get_gps_location(gps_uart, lcd_uart,loggingFileName,gps_start_time)
+    initial_time = time.ticks_ms()
 
-    # Main Loop
+    #Main Loop
     while True:
+        imu_start_time = time.ticks_ms()
+        
         latitude_LL = longitude_LL = latitude_GA = longitude_GA = 0
         latDivisor = lonDivisor = 1
-
-        # Check if GPS has position:
+        
+        #Check if GPS has position:
         str_array = gps_uart.readline()
         if not str_array:
             pass
         else:
             try:
-                str_array = str_array.decode("utf-8").strip().split(",")  # Decodes GPS input
-                # print(str_array)                            # Prints GPS Output
+                str_array = str_array.decode("utf-8").strip().split(",")      # Decodes GPS input
+                #print(str_array)                            # Prints GPS Output
                 if str_array[0] == '$GPGLL':
                     latitude_LL = get_latitude(str_array, 1)
                     longitude_LL = get_longitude(str_array, 3)
-                    # lcd_uart.write("in GNGLL")
-                    # print("in GPGLL: Latitude: ", latitude + "  Longitude: ", longitude)
+                    #lcd_uart.write("in GNGLL")
+                    #print("in GPGLL: Latitude: ", latitude + "  Longitude: ", longitude)
 
                 elif str_array[0] == '$GPGGA':
                     latitude_GA = get_latitude(str_array, 2)
                     longitude_GA = get_longitude(str_array, 4)
-                    # lcd_uart.write("in GNGGA")
-                    # print("in GPGGA: Latitude: ", latitude  + "  Longitude: ", longitude)
-
-                if ((latitude_LL or latitude_GA) and (longitude_LL or longitude_GA)):
+                    #lcd_uart.write("in GNGGA")
+                    #print("in GPGGA: Latitude: ", latitude  + "  Longitude: ", longitude)
+                
+                if((latitude_LL or latitude_GA) and (longitude_LL or longitude_GA)):
                     if (latitude_LL and latitude_GA):
                         latDivisor = 2
-
+                    
                     if (longitude_LL and longitude_GA):
                         lonDivisor = 2
-
-                    latitude_avg = (latitude_LL + latitude_GA) / latDivisor
-                    longitude_avg = (longitude_LL + longitude_GA) / lonDivisor
-
+                    
+                    new_latitude_avg = (latitude_LL + latitude_GA) / latDivisor
+                    new_longitude_avg = (longitude_LL + longitude_GA) / lonDivisor
+                    if(math.fabs(new_latitude_avg - latitude_avg) < 0.05 and abs(new_longitude_avg - longitude_avg) < 0.05):
+                        latitude_avg=new_latitude_avg
+                        longitude_avg=new_longitude_avg
+                
                     print(f'''
         GPS UPDATE\n
         Latitude: {latitude_avg:.10f}   Longitude: {longitude_avg:.10f}\n
         Raw Data: {str_array}\n
-        GPS UPDATE TIME: {time.ticks_ms() - gps_start_time}ms\n
+        GPS UPDATE TIME: {time.ticks_ms()-gps_start_time}ms\n
                         ''')
-
-                    # logging into text file
-                    # TODO: test
-
+                    
+                    #logging into text file 
+                    #TODO: test
+                        
                     with open(loggingFileName, "a") as file:
                         file.write("GPS UPDATE\n\n")
                         file.write(f"Latitude: {latitude_avg:.10f}   Longitude: {longitude_avg:.10f}\n\n")
                         file.write(f"Raw Data: {str_array}\n\n")
-                        file.write(f"GPS UPDATE TIME: {time.ticks_ms() - gps_start_time}ms\n")
-
+                        file.write(f"GPS UPDATE TIME: {time.ticks_ms()-gps_start_time}ms\n")
+                    
                     gps_start_time = time.ticks_ms()
             except (ValueError, IndexError):
                 lcd_uart.write(b"Error No Signal                 ")  # For 16x2 LCD
                 print("valueError: Likely no signal from being inside, no GPS antenna connected, or a broken wire")
-
-        imu_start_time = time.ticks_ms()
-        latitude_avg, longitude_avg, velocity_x, velocity_y = imu_update(latitude_avg, longitude_avg,
-                                                                         (time.ticks_ms() - imu_start_time) / 1_000,
-                                                                         velocity_x, velocity_y)
+        
+        
         update_time = time.ticks_ms() - imu_start_time
+        latitude_avg, longitude_avg, velocity_x, velocity_y = imu_update(latitude_avg, longitude_avg, update_time, velocity_x, velocity_y, sensor, loggingFileName)
         print(f'''IMU update time: {update_time} ms \nIMU refresh rate: {1000 / update_time} Hz''')
 
         if is_within_polygon(outerPolygon, (float(latitude_avg), float(longitude_avg))) is True and is_within_polygon(
@@ -165,4 +166,7 @@ if __name__ == '__main__':
             lcd_uart.write(b"OUT                             ")  # For 16x2 LCD
             print("\nKart is out of bounds\n")
             relay_on.value(0)
+            with open(loggingFileName, 'a') as file:
+                file.write(f"\n\n\ntime to glitch: {(time.ticks_ms()-initial_time)/1000} secs")
+                print(f"\n\ntime to glitch: {(time.ticks_ms()-initial_time)/1000} secs")
             break
